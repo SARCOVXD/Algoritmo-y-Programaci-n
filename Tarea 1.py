186380af7a41ddb14b0f77daab9d36a7dc592dfe
SARCOVXD <168433809+SARCOVXD@users.noreply.github.com>
2024-04-29 07:45:13 -0400

    Add files via upload

---
+++ Tarea 1.py
@@ -0,0 +1,7 @@
+print("Volumen de una arepa")
+print("¿Cual es la cantidad de ingredientes que hace falta para una arepa")
+agua = int(input("Cantidad de agua"))
+sal = int(input("Cantidad de sal"))
+harina = int(input("Cantidad de harina"))
+Total = sal + harina + agua
+print("El volumen de una arepa es:" + str(Total))
